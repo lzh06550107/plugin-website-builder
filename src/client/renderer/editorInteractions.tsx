0f88128556a513b830a89e88b34b4ac2f80e4cb8
LiaoZhongHao <liao08022040@gmail.com>
2026-09-17 18:22:32 +0800

    feat: add editor interaction context

---
+++ src/client/renderer/editorInteractions.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export interface WebsiteEditorInteractions {
+  onInlineTextCommit?: (nodeId: string, text: string) => void;
+}
+
+const WebsiteEditorInteractionsContext = React.createContext<WebsiteEditorInteractions>({});
+
+export interface WebsiteEditorInteractionsProviderProps extends WebsiteEditorInteractions {
+  children: React.ReactNode;
+}
+
+export function WebsiteEditorInteractionsProvider({
+  onInlineTextCommit,
+  children,
+}: WebsiteEditorInteractionsProviderProps) {
+  const value = React.useMemo(() => ({ onInlineTextCommit }), [onInlineTextCommit]);
+  return (
+    <WebsiteEditorInteractionsContext.Provider value={value}>
+      {children}
+    </WebsiteEditorInteractionsContext.Provider>
+  );
+}
+
+export function useWebsiteEditorInteractions() {
+  return React.useContext(WebsiteEditorInteractionsContext);
+}
