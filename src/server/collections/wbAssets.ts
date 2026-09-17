3ce45bf749fc3482b35d6f60fbd7bf0edd232382
LiaoZhongHao <liao08022040@gmail.com>
2026-09-17 20:40:23 +0800

    fix: define standard website asset file fields

--- src/server/collections/wbAssets.ts
+++ src/server/collections/wbAssets.ts
@@ -3,5 +3,30 @@ import { defineCollection } from '@nocobase/database';
 export default defineCollection({
   name: 'wbAssets',
   title: 'Website Builder Assets',
+  dataCategory: 'business',
+  createdBy: true,
+  updatedBy: true,
   template: 'file',
+  fields: [
+    { type: 'string', name: 'title' },
+    { type: 'string', name: 'filename' },
+    { type: 'string', name: 'extname' },
+    { type: 'integer', name: 'size' },
+    { type: 'string', name: 'mimetype' },
+    { type: 'text', name: 'path' },
+    { type: 'text', name: 'url' },
+    { type: 'text', name: 'preview' },
+    {
+      type: 'belongsTo',
+      name: 'storage',
+      target: 'storages',
+      foreignKey: 'storageId',
+    },
+    {
+      type: 'jsonb',
+      name: 'meta',
+      deletable: false,
+      defaultValue: {},
+    },
+  ],
 });
