5f08d794134937970453a474d2065c098117bc45
LiaoZhongHao <liao08022040@gmail.com>
2026-09-17 20:36:33 +0800

    fix: retry website asset authorizer after plugin load

--- src/server/plugin.ts
+++ src/server/plugin.ts
@@ -4,14 +4,22 @@ import { registerWebsiteAssetFileAccess } from './assets/fileAccess';
 import { registerWebsiteBuilderResource } from './resources/websiteBuilder';
 
 export class PluginWebsiteBuilderServer extends Plugin {
+  protected assetFileAccessRegistered = false;
+
   async afterAdd() {}
 
   async beforeLoad() {}
 
+  protected registerAssetFileAccess() {
+    if (this.assetFileAccessRegistered) return;
+    this.assetFileAccessRegistered = registerWebsiteAssetFileAccess(this.app);
+  }
+
   async load() {
     registerWebsiteBuilderResource(this.app);
     registerWebsiteBuilderAcl(this.app);
-    registerWebsiteAssetFileAccess(this.app);
+    this.registerAssetFileAccess();
+    this.app.on('afterLoad', () => this.registerAssetFileAccess());
   }
 
   async install() {}
