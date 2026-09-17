54500fea2900a855616256a3b3ce3cd7dd13daa5
LiaoZhongHao <liao08022040@gmail.com>
2026-09-17 20:23:31 +0800

    feat: add website asset ACL actions

--- src/server/acl/index.ts
+++ src/server/acl/index.ts
@@ -2,44 +2,54 @@ export const WEBSITE_BUILDER_VIEW_SNIPPET = 'pm.website-builder.view';
 export const WEBSITE_BUILDER_EDIT_SNIPPET = 'pm.website-builder.edit';
 export const WEBSITE_BUILDER_PUBLISH_SNIPPET = 'pm.website-builder.publish';
 
+export function getWebsiteBuilderAclDefinitions() {
+  return [
+    {
+      name: WEBSITE_BUILDER_VIEW_SNIPPET,
+      actions: [
+        'wbSites:list',
+        'wbSites:get',
+        'wbPages:list',
+        'wbPages:get',
+        'wbThemes:list',
+        'wbThemes:get',
+        'wbPageVersions:list',
+        'wbPageVersions:get',
+        'wbAssets:list',
+        'wbAssets:get',
+        'websiteBuilder:getDraft',
+        'websiteBuilder:getPublished',
+      ],
+    },
+    {
+      name: WEBSITE_BUILDER_EDIT_SNIPPET,
+      actions: [
+        'wbSites:create',
+        'wbSites:update',
+        'wbSites:destroy',
+        'wbPages:create',
+        'wbPages:update',
+        'wbPages:destroy',
+        'wbThemes:create',
+        'wbThemes:update',
+        'wbThemes:destroy',
+        'wbAssets:create',
+        'websiteBuilder:saveDraft',
+      ],
+    },
+    {
+      name: WEBSITE_BUILDER_PUBLISH_SNIPPET,
+      actions: ['websiteBuilder:publish'],
+    },
+  ];
+}
+
 export function registerWebsiteBuilderAcl(app: any) {
-  app.acl.registerSnippet({
-    name: WEBSITE_BUILDER_VIEW_SNIPPET,
-    actions: [
-      'wbSites:list',
-      'wbSites:get',
-      'wbPages:list',
-      'wbPages:get',
-      'wbThemes:list',
-      'wbThemes:get',
-      'wbPageVersions:list',
-      'wbPageVersions:get',
-      'websiteBuilder:getDraft',
-      'websiteBuilder:getPublished',
-    ],
-  });
-
-  app.acl.registerSnippet({
-    name: WEBSITE_BUILDER_EDIT_SNIPPET,
-    actions: [
-      'wbSites:create',
-      'wbSites:update',
-      'wbSites:destroy',
-      'wbPages:create',
-      'wbPages:update',
-      'wbPages:destroy',
-      'wbThemes:create',
-      'wbThemes:update',
-      'wbThemes:destroy',
-      'websiteBuilder:saveDraft',
-    ],
-  });
-
-  app.acl.registerSnippet({
-    name: WEBSITE_BUILDER_PUBLISH_SNIPPET,
-    actions: ['websiteBuilder:publish'],
-  });
+  for (const definition of getWebsiteBuilderAclDefinitions()) {
+    app.acl.registerSnippet(definition);
+  }
 
   // Published website data is intentionally public; draft and management APIs are not.
+  // Asset files are authorized separately by File Manager's file-access authorizer.
   app.acl.allow('websiteBuilder', 'getPublishedByPath', 'public');
 }
